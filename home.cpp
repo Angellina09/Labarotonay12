9bbd4b692d2244a80a75c87d35afae1fdbeb8392
Angellina09 <168266959+Angellina09@users.noreply.github.com>
2024-04-29 14:34:59 +0500

    Create home.cpp

---
+++ home.cpp
@@ -0,0 +1,17 @@
+#include "graphics.h"
+#include "picture.hpp"
+void home () {
+setcolor(BLACK);
+   int polypoints[8] = {500, 250, 625, 150, 750, 250};
+   setfillstyle(1, COLOR(61, 32, 11));
+   bar(500, 250, 750, 400);
+   setfillstyle(1, COLOR(36, 15, 0));
+   fillpoly(3, polypoints);
+   setfillstyle(1, COLOR(245, 223, 24)); 
+   fillellipse(625, 200, 20, 20);
+   setfillstyle(1, COLOR(245, 223, 24));
+   bar(530, 280, 600, 360);
+   setcolor(BLACK);
+   line(530, 320, 600, 320);
+   line(565, 280, 565, 360);
+}

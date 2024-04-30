dc45d74882f142c256165738148acda9298d2651
Angellina09 <168266959+Angellina09@users.noreply.github.com>
2024-04-30 11:19:40 +0500

    manv1.0

---
+++ man.cpp
@@ -0,0 +1,13 @@
+#include <graphics.h>
+#include <picture.hpp>
+void man() {
+    setfillstyle(1, LIGHTGRAY);
+    line(173, 550, 130, 595);
+    line(177, 550, 220, 595);
+    fillellipse(175, 525, 25, 25);
+    fillellipse(175, 590, 25, 40);
+    line(175, 630, 140, 680);
+    line(175, 630, 210, 680);
+}
+}
+

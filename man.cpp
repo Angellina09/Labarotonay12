cab3b940bf36136de34497233ee60ef866aab33d
Angellina09 <168266959+Angellina09@users.noreply.github.com>
2024-04-30 13:37:43 +0500

    Update man.cpp

--- man.cpp
+++ man.cpp
@@ -2,12 +2,12 @@
 #include <picture.hpp>
 void man() {
     setfillstyle(1, LIGHTGRAY);
-    line(173, 550, 130, 595);
-    line(177, 550, 220, 595);
-    fillellipse(175, 525, 25, 25);
-    fillellipse(175, 590, 25, 40);
-    line(175, 630, 140, 680);
-    line(175, 630, 210, 680);
+    line(173, 450, 130, 495);
+    line(177, 450, 220, 495);
+    fillellipse(175, 425, 25, 25);
+    fillellipse(175, 490, 25, 40);
+    line(175, 530, 140, 580);
+    line(175, 530, 210, 580);
 }
 
 
